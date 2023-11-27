e785420bce0111cfc2a2cc9d119fb52c026fea1b
Davidson <vader@assoweb.com.br>
2023-11-27 10:35:15 -0300

    fix: Identation category layout file

--- src/app/categoria/[slug]/layout.tsx
+++ src/app/categoria/[slug]/layout.tsx
@@ -1,17 +1,17 @@
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-	title: {
-		default: 'Blog',
-		template: 'Categoria %s | WordPress Next Blog',
-	},
-	description: 'Created by Davidson',
+  title: {
+    default: 'Blog',
+    template: 'Categoria %s | WordPress Next Blog',
+  },
+  description: 'Created by Davidson',
 };
 
 export default function BlogCategoryLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return <>{children}</>;
+  return <>{children}</>;
 }
